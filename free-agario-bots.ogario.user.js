cd1b61301b4d78302c6891a6e9da4fcfc4622117
GhostArmyYT324 <56448985+GhostArmyYT324@users.noreply.github.com>
2019-10-26 14:28:25 +0200

    free-agario-bots with no captcha

--- free-agario-bots.ogario.user.js
+++ free-agario-bots.ogario.user.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Free Agar.io Bots (OGARio Version)
-// @version      1.0.9
+// @version       1.0.10
 // @description  Free open source agar.io bots with OGARio
 // @author       Nel & szymy (OGARio deobfuscated by ReF)
 // @grant        GM_xmlhttpRequest
@@ -21,7 +21,7 @@ if(location.host === 'agar.io' && location.pathname === '/'){
 function modifyHTML(html){
     return html
         .replace('<head>', '<head><script src="https://bundle.run/buffer@5.2.1"></script><script src="https://pastebin.com/raw/z9hBsFYi"></script>')
-        .replace('https://cdn.ogario.ovh/v4/beta/ogario.v4.js', 'https://pastebin.com/raw/qguS2wyP')
+        .replace('https://cdn.ogario.ovh/v4/beta/ogario.v4.js', 'https://pastebin.com/raw/BQiT9cgW')
 }
 
 if(!navigator.userAgent.includes('Chrome/') || Number(navigator.userAgent.match(/Chrome\/(\d+)/)[1]) < 76) window.stop()
